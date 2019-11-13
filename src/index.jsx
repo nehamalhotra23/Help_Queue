61373e0819286cfb6909e5f16d0466004db73ce4
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-11-12 23:22:12 -0800

    add hot loading

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
+import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.render(
-  <div>
-    <h1>Help Queue</h1>
-    <h3>3A</h3>
-    <h3>Thato and Haley</h3>
-    <h3><em>Firebase will not save record!</em></h3>
-  </div>,
-  document.getElementById('react-app-root')
-);
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('react-app-root')
+  );
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    render(App)
+  });
+}
