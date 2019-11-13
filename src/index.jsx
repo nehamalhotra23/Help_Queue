4b23d67aa63884ff3e8a6288c6b41adbbfe447b4
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-11-12 22:16:20 -0800

    react-help-queue create initial project w/ webpack, babel, and react

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Help Queue</h1>
+    <h3>3A</h3>
+    <h3>Thato and Haley</h3>
+    <h3><em>Firebase will not save record!</em></h3>
+  </div>,
+  document.getElementById('react-app-root')
+);
