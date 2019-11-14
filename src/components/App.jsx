08cd3b57e9eadd9798356a2f402e46cfd4d7eac9
Student <example@example.com>
2019-11-14 11:23:01 -0800

    add image component and routing to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,13 @@ import Header from './Header';
 import TicketList from './TicketList';
 import { Switch, Route } from 'react-router-dom';
 import NewTicketForm from './NewTicketForm';
+import Image from './Image';
 
 function App() {
   return (
     <div>
       <Header/>
+      <Image/>
       <Switch>
         <Route exact path='/' component={TicketList} />
         <Route path='/newticket' component={NewTicketForm} />
---
+++ src/components/Image.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import clippy from '../assets/images/clippy.png';
+
+function Image(){
+  return (
+    <div>
+      <img src={clippy}/>
+    </div>
+  );
+}
+
+export default Image;
